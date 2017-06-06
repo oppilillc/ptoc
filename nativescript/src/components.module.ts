0294e1a1fb13218b4bb1953ae0be5fb115337282
Sangeetha Manivannan <Sangeetha@MacBook-Pro.fios-router.home>
2017-06-05 21:50:30 -0400

    initial commit

---
+++ nativescript/src/components.module.ts
@@ -0,0 +1,76 @@
+// nativescript
+import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
+import { NativeScriptFormsModule } from 'nativescript-angular/forms';
+import { NativeScriptHttpModule } from 'nativescript-angular/http';
+import { NativeScriptRouterModule } from 'nativescript-angular/router';
+import { Http } from '@angular/http';
+
+// angular
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+
+// libs
+import { TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+// app
+import { AppComponent } from './app/components/app.component';
+import { AboutComponent } from './app/components/about/about.component';
+import { HomeComponent } from './app/components/home/home.component';
+import { routes } from './app/components/app.routes';
+
+// feature modules
+import { CoreModule } from './app/shared/core/core.module';
+import { AnalyticsModule } from './app/shared/analytics/analytics.module';
+import { MultilingualModule, translateLoaderFactory } from './app/shared/i18n/multilingual.module';
+import { SampleModule } from './app/shared/sample/sample.module';
+import { ConsoleService, ConsoleTarget, LogLevel } from './app/shared/core/index';
+
+// intermediate component module
+// helps encapsulate custom native modules in with the components
+// note: couple ways this could be done, just one option presented here...
+@NgModule({
+  imports: [
+    NativeScriptModule,
+    NativeScriptFormsModule,
+    NativeScriptHttpModule,
+    NativeScriptRouterModule,
+    AnalyticsModule,
+    CoreModule,
+    MultilingualModule.forRoot([{
+      provide: TranslateLoader,
+      deps: [Http],
+      useFactory: (translateLoaderFactory)
+    }]),
+    SampleModule
+  ],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent
+  ],
+  schemas: [
+    NO_ERRORS_SCHEMA,
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
+  exports: [
+    NativeScriptModule,
+    NativeScriptFormsModule,
+    NativeScriptHttpModule,
+    NativeScriptRouterModule,
+    MultilingualModule,
+    AppComponent,
+    AnalyticsModule,
+    CoreModule,
+    SampleModule
+  ]
+})
+export class ComponentsModule { }
+
+// For AoT compilation to work:
+export function cons() {
+  return console;
+}
+
+export function consoleLogTarget(service: ConsoleService) {
+  return new ConsoleTarget(service, { minLogLevel: LogLevel.Debug });
+}
---
+++ src/client/app/components/home/home.component.ts
@@ -0,0 +1,108 @@
+// libs
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+
+// app
+import { RouterExtensions, Config } from '../../shared/core/index';
+import { IAppState, getNames } from '../../shared/ngrx/index';
+import * as nameList from '../../shared/sample/index';
+
+@Component({
+  moduleId: module.id,
+  selector: 'sd-home',
+  templateUrl: 'home.component.html',
+  styleUrls: ['home.component.css']
+})
+export class HomeComponent implements OnInit {
+  public names$: Observable<any>;
+  public newName: string;
+
+  public beginBalance: number;
+  public accrualPeriod: number ;
+  public accrualRate: number;
+  public desiredDate: Date;
+  public asOfDate: Date;
+  public newBalance: number;
+  public days: number;
+  public ptoBalance: number;
+  public desiredWeekends: Date[] = [];
+  public asOfDays: Date[] = [];
+  public selectedDesiredDay: number = 0; 
+  public selectedAsOfDay: number = 0;
+  
+
+  constructor(private store: Store<IAppState>, public routerext: RouterExtensions) {}
+
+  ngOnInit() {
+    this.newName = '';
+    this.ptoBalance = 0;
+    var today = new Date(); 
+    today.setDate( today.getDate() + ( 6 - today.getDay() )); 
+    this.desiredWeekends.push(new Date(today));
+    var tempDay = new Date(today);
+    for(var i=0; i<5;i++) {
+    	tempDay.setDate(tempDay.getDate() + 7); 
+	this.desiredWeekends.push(new Date(tempDay));
+    }
+    tempDay = new Date(today);
+    console.log("tempDay:", tempDay);
+    console.log("today:", today);
+    this.selectedDesiredDay = 0;
+    for(var i=0; i<5;i++) {
+    	tempDay.setDate(tempDay.getDate() - 7); 
+	this.asOfDays.push(new Date(tempDay));
+    }
+    this.asOfDays = this.asOfDays.slice().reverse();
+    this.selectedAsOfDay = this.asOfDays.length -1; 
+    console.log("selectedDesiredDay:", this.selectedDesiredDay);
+    console.log("selectedAsOfDay:", this.selectedAsOfDay);
+  }
+
+  /*
+   * @param newname  any text as input.
+   * @returns return false to prevent default form submit behavior to refresh the page.
+   */
+  calculatePTO(): boolean {
+  console.log("as of date:", this.asOfDate);
+  console.log("desired date:", this.desiredDate);
+    this.days = this.dateDiff(this.asOfDate, this.desiredDate);
+    this.newBalance = (this.accrualRate * this.days / this.accrualPeriod);
+    this.ptoBalance = +this.beginBalance + +this.newBalance;
+
+    return false;
+  }
+  desiredDaysChanged(picker) {
+  	this.desiredDate = this.desiredWeekends[picker.selectedDesiredDay];
+	this.calculatePTO();
+  }
+  asOfDaysChanged(asOfDayPicker) {
+	console.log("81: selectedAsOfDay:", asOfDayPicker.selectedAsOfDay);
+	console.log("this.asOfDate:", this.asOfDays[asOfDayPicker.selectedAsOfDay]);
+  	this.asOfDate = this.asOfDays[asOfDayPicker.selectedAsOfDay];
+	this.calculatePTO();
+  }
+  addName(): boolean {
+    this.store.dispatch(new nameList.AddAction(this.newName));
+    this.newName = '';
+    return false;
+  }
+  dateDiff(date1, date2){
+    var d1 = new Date(date1); 
+    var d2 = new Date(date2); 
+    var milliseconds = +d2 - +d1; 
+    var days = milliseconds / (86400000); 
+    return days;
+  }
+ 
+  readAbout() {
+    // Try this in the {N} app
+    // {N} can use these animation options
+    this.routerext.navigate(['/about'], {
+      transition: {
+        duration: 1000,
+        name: 'slideTop',
+      }
+    });
+  }
+  }
