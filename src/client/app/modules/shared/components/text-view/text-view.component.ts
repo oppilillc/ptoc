450b3e4eb0b82faa43edcd48c310ab3ffccd572f
Sangeetha Manivannan <Sangeetha@MacBook-Pro.fios-router.home>
2017-11-13 18:06:45 -0500

    Completed initial structure of components.

--- src/client/app/modules/shared/components/text-view/text-view.component.ts
+++ src/client/app/modules/shared/components/text-view/text-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   moduleId: module.id,
@@ -9,4 +9,8 @@ import { Component, Input } from '@angular/core';
 export class TextViewComponent {
   @Input() text: string;
    @Input() hint: string;
+   @Output() value = new EventEmitter(); 
+   change(newValue) {
+   	this.value.emit(newValue); 
+   }
 }
